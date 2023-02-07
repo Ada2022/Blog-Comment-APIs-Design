4582c023cc090ac285d19e0cef69f1f93fef1f67
Ada2022 <hao_ouyang@berkeley.edu>
2023-02-07 04:44:02 -0800

    blog comment api design

---
+++ app.py
@@ -0,0 +1,88 @@
+import os
+from process_function import *
+from flask import Flask, request, render_template
+
+DB_FILE = "./blogs.db"
+app = Flask('myApp')
+
+
+@app.route("/", methods=["GET", "POST", "DELETE"])
+def home():
+    if request.method == "POST" or request.method == "GET":
+        blog_id = request.form.get('blog_id')
+        author = request.form.get('author')
+        content = request.form.get('content')
+        function = request.form.get('function')
+
+        if function == 'add_blog':
+            add_blog_api(blog_id, author, content)
+            return "Blog added successfully. Please click the arrow in the upper left corner to return to the main interface"
+        elif function == 'show_blog':
+            data = show_blog_api(blog_id)
+            return render_template("./home.html", function=function, data=data)
+        elif function == 'add_comment':
+            blog_exit_flag = add_comment_api(blog_id, author, content)
+            if blog_exit_flag:
+                return "Comment added successfully. Please click the arrow in the upper left corner to return to the main interface"
+            else:
+                return "Blog you want to comment on does not exist. Please check the Blog ID"
+        elif function == 'show_comment':
+            data = show_comment_api(blog_id, author)
+            return render_template("./home.html", function=function, data=data)
+        elif function == 'delete_comment':
+            delete_comment_api(blog_id, author)
+            return "Comment deleted successfully. Please click the arrow in the upper left corner to return to the main interface"
+
+    return render_template("./home.html")
+
+
+@app.route("/", methods=["POST"])
+def add_blog_api(blog_id, author, content):
+    conn = sqlite3.connect(DB_FILE)
+    add_blog(conn, blog_id, author, content)
+    return "Blog Added", 200
+
+
+@app.route("/", methods=["GET"])
+def show_blog_api(blog_id):
+    blog_id = request.args.get("blog_id")
+    conn = sqlite3.connect(DB_FILE)
+    data = show_blog(conn, blog_id=blog_id)
+    return data
+
+
+@app.route("/", methods=["POST"])
+def add_comment_api(blog_id, author, content):
+    conn = sqlite3.connect(DB_FILE)
+    blog_exit_flag = add_comment(conn, blog_id, author, content)
+    return blog_exit_flag
+
+
+@app.route("/", methods=["GET"])
+def show_comment_api(blog_id, author):
+    blog_id = request.args.get("blog_id")
+    author = request.args.get("author")
+    conn = sqlite3.connect(DB_FILE)
+    data = show_comment(conn, blog_id, author)
+    return data
+
+
+@app.route("/", methods=["DELETE"])
+def delete_comment_api(blog_id, author):
+    blog_id = request.args.get("blog_id")
+    author = request.args.get("author")
+    conn = sqlite3.connect(DB_FILE)
+    delete_comment(conn, blog_id, author)
+    return "Comment Deleted", 200
+
+
+if __name__ == "__main__":
+    database_path = "./blogs.db"
+    if not os.path.exists(database_path):
+        conn = sqlite3.connect(DB_FILE)
+        initDB(conn)
+        print("DB initialized.")
+        app.run()
+        conn.close()
+    else:
+        app.run()
---
+++ process_function.py
@@ -0,0 +1,177 @@
+import sqlite3
+import pandas as pd
+from datetime import datetime
+import argparse
+
+
+def initDB(conn):
+    cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS blogs (
+        id INTEGER PRIMARY KEY,
+        blog_id INTERGER NOT NULL,
+        author TEXT NOT NULL,
+        content TEXT NOT NULL
+    )
+    """)
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS comments (
+        id INTEGER PRIMARY KEY,
+        blog_id INTEGER NOT NULL,
+        author TEXT NOT NULL,
+        time_stamp TEXT NOT NULL,
+        content TEXT NOT NULL,
+        FOREIGN KEY (blog_id) REFERENCES blogs(blog_id)
+    )
+    """)
+    conn.commit()
+
+
+def add_comment(conn, blog_id, author, content):
+    cursor = conn.cursor()
+
+    blog_exist = pd.read_sql_query(
+        "SELECT blog_id FROM blogs WHERE blog_id = ? ", conn, params=(blog_id, ))
+    if blog_exist.empty:
+        print("Blog doesn't exist, cannot comment")
+        return False
+
+    last_id = pd.read_sql_query(
+        "SELECT * FROM comments WHERE blog_id = ? ", conn, params=(blog_id, ))
+    last_id = -1 if last_id.empty else last_id.index[-1]
+
+    cursor.execute("""
+    INSERT INTO comments (id, blog_id, author, time_stamp, content)
+    VALUES (?, ?, ?, ?, ?)
+    """, (last_id + 1, blog_id, author, datetime.now().strftime('%Y-%m-%d %H:%M'), content))
+
+    conn.commit()
+    return True
+
+
+def show_comment(conn, blog_id=None, author=None, time_stamp=None):
+    query = "SELECT * FROM comments"
+    conditions = []
+    params = []
+    if author is not None:
+        conditions.append("author = ?")
+        params.append(author)
+    if blog_id is not None:
+        conditions.append("blog_id = ?")
+        params.append(blog_id)
+    if time_stamp is not None:
+        conditions.append("time_stamp = ?")
+        params.append(time_stamp)
+    if conditions:
+        query += " WHERE " + " AND ".join(conditions)
+
+    cursor = conn.cursor()
+    cursor.execute(query, params)
+    comments = cursor.fetchall()
+
+    if len(comments) == 0:
+        print("No matching comments")
+    else:
+        print("\nComments:")
+        for comment in comments:
+            print(comment)
+    return comments
+
+
+def delete_comment(conn, blog_id=None, author=None, time_stamp=None):
+    query = "DELETE FROM comments"
+    conditions = []
+    params = []
+    if author is not None:
+        conditions.append("author = ?")
+        params.append(author)
+    if blog_id is not None:
+        conditions.append("blog_id = ?")
+        params.append(blog_id)
+    if time_stamp is not None:
+        conditions.append("time_stamp = ?")
+        params.append(time_stamp)
+    if conditions:
+        query += " WHERE " + " AND ".join(conditions)
+
+    cursor = conn.cursor()
+    cursor.execute(query, params)
+    conn.commit()
+
+
+def add_blog(conn, blog_id, author, content):
+    cursor = conn.cursor()
+
+    last_id = pd.read_sql_query(
+        "SELECT blog_id FROM blogs", conn)
+    last_id = -1 if last_id.empty else last_id.index[-1]
+
+    cursor.execute("""
+    INSERT INTO blogs (id, blog_id, author, content)
+    VALUES (?, ?, ?, ?)
+    """, (last_id + 1, blog_id, author, content))
+
+    conn.commit()
+
+
+def show_blog(conn, blog_id=None, author=None):
+    query = "SELECT * FROM blogs"
+    conditions = []
+    params = []
+    if blog_id is not None:
+        conditions.append("blog_id = ?")
+        params.append(blog_id)
+    if author is not None:
+        conditions.append("author = ?")
+        params.append(author)
+    if conditions:
+        query += " WHERE " + " AND ".join(conditions)
+
+    cursor = conn.cursor()
+    cursor.execute(query, params)
+    blogs = cursor.fetchall()
+    if len(blogs) == 0:
+        print("No matching blogs")
+    else:
+        print("\nBlogs:")
+        for blog in blogs:
+            print(blog)
+    return blogs
+
+
+def process_args(args):
+    db = args.database_name
+    conn = sqlite3.connect(db)
+
+    initDB(conn)
+    if args.command == "add-blog":
+        add_blog(conn, args.blog_id, args.author, args.content)
+    elif args.command == "show-blog":
+        show_blog(conn, blog_id=args.blog_id, author=args.author)
+    elif args.command == "add-comment":
+        add_comment(conn, args.blog_id, args.author, args.content)
+    elif args.command == "delete-comment":
+        delete_comment(conn, blog_id=args.blog_id,
+                       author=args.author, time_stamp=args.time_stamp)
+    elif args.command == "show-comment":
+        show_comment(conn, blog_id=args.blog_id,
+                     author=args.author, time_stamp=args.time_stamp)
+    else:
+        print("Invalid command")
+
+    conn.close()
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Blog Comment System")
+    parser.add_argument('--database_name', default="blogs.db",
+                        help="please enter database name")
+    parser.add_argument("command", choices=[
+                        "add-blog", "add-comment", "delete-comment", "show-comment", "show-blog"], help="Action to be taken")
+    parser.add_argument("--blog-id", type=int, help="Blog ID")
+    parser.add_argument("--author", help="Author of the blog/comment")
+    parser.add_argument("--content", help="Content of the blog/comment")
+    parser.add_argument("--time-stamp", help="Time stamp of the comment")
+    args = parser.parse_args()
+    process_args(args)
